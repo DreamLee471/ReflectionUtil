b21e6e32e7ae15ab4aa9138d45e26387cfc8dc02
dreamlee.lw <dreamlee.lw@alibaba-inc.com>
2015-10-01 09:35:55 +0800

    第一次提交

---
+++ src/main/java/com/alibaba/common/reflection/ReflectionUtil/App.java
@@ -0,0 +1,13 @@
+package com.alibaba.common.reflection.ReflectionUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
